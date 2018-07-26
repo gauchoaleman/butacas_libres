e2ee2b48e7fdef0c1b76b5156cd43771d4fa9f93
Stefan Ronacher <stefan.ronacher.78@gmail.com>
2018-07-26 11:09:46 -0300

    2do commit

--- src/edu/cientoonce/Main.java
+++ src/edu/cientoonce/Main.java
@@ -7,16 +7,16 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int i;
-        int[] a =  new boolean[][5];
+        int[] a =  new int[5];
         int vacias=0;
 
         for(i=0;i<a.length;i++){
-            System.out.println("Ingrese elemento "+(i+1)+" :");
+            System.out.println("Ingrese butaca "+(i+1)+":");
             Scanner sc = new Scanner(System.in);
             a[i]=sc.nextInt();
         }
         for(i=0;i<a.length;i++){
-            if( a[i]==false)
+            if( a[i]==0)
                 vacias++;
         }
         System.out.println("Ctd. de butacas vacias: "+vacias);
